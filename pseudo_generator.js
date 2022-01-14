4d1be2bb0e9ee71859335baa53817a25bf0deba6
Joss <josselin.bnd@gmail.com>
2022-01-14 10:26:09 +0100

    clean code

--- pseudo_generator.js
+++ pseudo_generator.js
@@ -7,30 +7,12 @@ function generateSyllable() {
     return consonant[i1] + vowel[i2];
 }
 
-function generateSyllableBatch(batchSize) {
-    const batch = [];
-
-    for (let i = 0; i < batchSize; i++) {
-        batch.push(generateSyllable());
-    }
-
-    return batch;
-}
-
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-function generateName() {
-    const batch1 = generateSyllableBatch(20);
-    const batch2 = generateSyllableBatch(20);
-    const batch3 = generateSyllableBatch(20);
-
-    const i1 = Math.floor(Math.random() * batch1.length);
-    const i2 = Math.floor(Math.random() * batch2.length);
-    const i3 = Math.floor(Math.random() * batch3.length);
-    
-    return capitalizeFirstLetter(batch1[i1] + batch2[i2] + batch3[i3]);
+function generateName() {  
+    return capitalizeFirstLetter(generateSyllable() + generateSyllable() + generateSyllable());
 }
 
 console.log("Now, you'll be named: " + generateName())
